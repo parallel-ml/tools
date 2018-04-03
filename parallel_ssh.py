b5203dc9d2c15384cd011baf8253b06d139cce41
Ramyad <rhadidi@gatech.edu>
2018-04-03 17:18:31 -0400

    ip tools

---
+++ parallel_ssh.py
@@ -0,0 +1,33 @@
+#!/bin/python
+
+
+# BASH Version: $ pssh -t 5 -h ips -l pi -A -i "uptime"
+
+
+from pssh.pssh_client import ParallelSSHClient
+from pssh.utils import load_private_key
+
+hosts = []
+with open("ips", "r") as f:
+  for line in f:
+    hosts.append(line.rstrip())
+
+print(hosts)
+
+pkey = load_private_key('/Users/ramyadhadidi/.ssh/id_rsa_pis')
+
+client = ParallelSSHClient(hosts, user='pi', pkey=pkey)
+output = client.run_command('ls')
+
+
+for host, host_output in output.items(): 
+    for line in host_output.stdout:
+        print("Host [%s] - %s" % (host, line))
+
+'''
+for host, host_output in output.items():
+    print(host)
+    for line in host_output.stdout:
+        print(line)
+    print
+'''
