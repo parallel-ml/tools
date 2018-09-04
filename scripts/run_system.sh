2437b64658961c0e56b8888820606e8934d1148f
Jiashen Cao <caojiashen24@gmail.com>
2018-09-04 12:01:56 -0400

    fix for starting system on pi

--- scripts/run_system.sh
+++ scripts/run_system.sh
@@ -2,7 +2,7 @@
 # start server and waits a few time
 # if argument provided, then start the server
 if [[ -n $1 ]]; then
-    cd $HOME/conv && python -m system.server > $HOME/stats/"$(hostname)"
+    cd $HOME/conv && python -m system.server
 else
-    cd $HOME/conv && python -m system.client > $HOME/stats/"$(hostname)"
+    cd $HOME/conv && python -m system.client
 fi
